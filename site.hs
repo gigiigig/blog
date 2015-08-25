c770557a3b2372613369585ee2e818db067a3850
Luigi Antonini <luigi.antonini84@gmail.com>
2015-08-25 11:18:48 +0200

    Apply `Clean Blog` template.

--- site.hs
+++ site.hs
@@ -2,18 +2,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
-
+import Data.List (intersperse, isSuffixOf)
+import Data.List.Split (splitOn)
+import System.FilePath (combine, splitExtension, takeFileName)
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
+    match "img/*" $ do
         route   idRoute
         compile copyFileCompiler
 
     match "css/*" $ do
         route   idRoute
         compile compressCssCompiler
+   
+    match "js/*" $ do
+        route   idRoute
+        compile copyFileCompiler
 
     match (fromList ["about.rst", "contact.markdown"]) $ do
         route   $ setExtension "html"
@@ -22,25 +28,25 @@ main = hakyll $ do
             >>= relativizeUrls
 
     match "posts/*" $ do
-        route $ setExtension "html"
+        route $ directorizeDate `composeRoutes` setExtension "html" 
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
+    {-create ["archive.html"] $ do-}
+    {-    route idRoute-}
+    {-    compile $ do-}
+    {-        posts <- recentFirst =<< loadAll "posts/*"-}
+    {-        let archiveCtx =-}
+    {-                listField "posts" postCtx (return posts) `mappend`-}
+    {-                constField "title" "Archives"            `mappend`-}
+    {-                defaultContext-}
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+    {-        makeItem ""-}
+    {-            >>= loadAndApplyTemplate "templates/archive.html" archiveCtx-}
+    {-            >>= loadAndApplyTemplate "templates/default.html" archiveCtx-}
+    {-            >>= relativizeUrls-}
 
 
     match "index.html" $ do
@@ -49,7 +55,6 @@ main = hakyll $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
                     defaultContext
 
             getResourceBody
@@ -65,3 +70,12 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+directorizeDate :: Routes
+directorizeDate = customRoute (\i -> directorize $ toFilePath i)
+  where
+    directorize path = dirs ++ ext 
+      where
+        (dirs, ext) = splitExtension $ concat $
+          (intersperse "/" date) ++ ["/"] ++ (intersperse "-" rest)
+        (date, rest) = splitAt 3 $ splitOn "-" path
