f9e35cc73c8d418ad1e3afebb6cd893b8553a187
Luigi Antonini <luigi.antonini84@gmail.com>
2015-08-25 11:18:48 +0200

    Apply `Clean Blog` template.

--- site.hs
+++ site.hs
@@ -7,13 +7,17 @@ import           Hakyll
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
+    match "img/*" $ do
         route   idRoute
         compile copyFileCompiler
 
     match "css/*" $ do
         route   idRoute
         compile compressCssCompiler
+   
+    match "js/*" $ do
+        route   idRoute
+        compile copyFileCompiler
 
     match (fromList ["about.rst", "contact.markdown"]) $ do
         route   $ setExtension "html"
@@ -28,19 +32,19 @@ main = hakyll $ do
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
+    {-create ["archive.html"] $ do-}
+    {-    route idRoute-}
+    {-    compile $ do-}
+    {-        posts <- recentFirst =<< loadAll "posts/*"-}
+    {-        let archiveCtx =-}
+    {-                listField "posts" postCtx (return posts) `mappend`-}
+    {-                constField "title" "Archives"            `mappend`-}
+    {-                defaultContext-}
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+    {-        makeItem ""-}
+    {-            >>= loadAndApplyTemplate "templates/archive.html" archiveCtx-}
+    {-            >>= loadAndApplyTemplate "templates/default.html" archiveCtx-}
+    {-            >>= relativizeUrls-}
 
 
     match "index.html" $ do
@@ -65,3 +69,4 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
